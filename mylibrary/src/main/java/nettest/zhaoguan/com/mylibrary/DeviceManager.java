2246a18b9b7a2e3bfc831ff9a128cb24c548fab9
淡了点 <474608897@qq.com>
2018-06-08 16:15:16 +0800

    commit

--- mylibrary/src/main/java/nettest/zhaoguan/com/mylibrary/DeviceManager.java
+++ mylibrary/src/main/java/nettest/zhaoguan/com/mylibrary/DeviceManager.java
@@ -1,11 +1,16 @@
 package nettest.zhaoguan.com.mylibrary;
 
+import android.content.Context;
 import android.util.Log;
+
+import com.lt.volley.http.Volley;
 
 public class DeviceManager {
 
 
-    public static void test(){
+    public static void test(Context context){
         Log.i("DeviceManager", "test");
+
+        Volley.newRequestQueue(context);
     }
 }
