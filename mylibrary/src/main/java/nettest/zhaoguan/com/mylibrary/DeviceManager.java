d05e2ab7984c689eb4dba5addb2a5bb1d6934446
淡了点 <474608897@qq.com>
2018-06-08 15:45:50 +0800

    commit

--- mylibrary/src/main/java/nettest/zhaoguan/com/mylibrary/DeviceManager.java
+++ mylibrary/src/main/java/nettest/zhaoguan/com/mylibrary/DeviceManager.java
@@ -1,9 +1,11 @@
 package nettest.zhaoguan.com.mylibrary;
 
+import android.util.Log;
+
 public class DeviceManager {
 
 
     public static void test(){
-
+        Log.i("DeviceManager", "test");
     }
 }
